f7e6250166bd452a9dd7a48e09775229a374e8f3
EC2 Default User <ec2-user@ip-172-31-29-13.us-west-2.compute.internal>
2023-09-20 04:12:46 +0000

    install aws-cli on render

--- scripts/setup-render.sh
+++ scripts/setup-render.sh
@@ -1,10 +1,5 @@
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install -i /opt/render/project/src/aws-cli -b /opt/render/project/src/bin
 python3 -m pip install --upgrade pip
 python3 -m pip install flask gunicorn requests rocketry stripe boto3 pydantic==1.10.10
-
-# echo "SETTING UP SSH"
-# # todo: upload pem file somehow
-# # or create ssh key pair in code?
-# chmod 600 ./rave-key-1.pem
-# echo "StrictHostKeyChecking=accept-new" >> ~/.ssh/config
-# echo "" >> ~/.ssh/config
-# chmod 700 ~/.ssh && chmod 600 ~/.ssh/*
